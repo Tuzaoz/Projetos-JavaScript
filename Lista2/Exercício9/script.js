2c3322a5d960407c67de39a29d517606f7028e63
Arthur Dantas <arthurdantaspro@gmail.com>
2022-10-18 23:50:23 -0300

    Tabuada funcional

---
+++ Lista2/Exercício9/script.js
@@ -0,0 +1,14 @@
+var num = document.getElementById('numero')
+function Tabuada(){
+    for (let i=1; i<=10; i++){
+        var resultado = num.value*i
+        const para = document.createElement('p')
+        const res = document.createTextNode(num.value + ' x ' + i + ' = ' + resultado)
+        para.appendChild(res)
+
+        const tabuada = document.getElementById("resCamp")
+        tabuada.appendChild(para)
+        resultado = 0
+    }
+}
+
