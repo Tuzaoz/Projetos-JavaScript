a9414d1c6d836a20412c170bc07d9f1d86bf9b06
Arthur Dantas <arthurdantaspro@gmail.com>
2022-10-19 22:59:20 -0300

    Exercícios 1-3

---
+++ Lista2/Exercício3/script.js
@@ -0,0 +1,16 @@
+var num1 = prompt('Digite um Número:')
+var num2 = prompt('Digite Outro Número:')
+
+const resultado = document.getElementById('localresult')
+function imprime(){
+    if(num1 > num2){
+        resultado.innerHTML=(`${num1} é maior que ${num2}`)   
+    }
+    if(num2 > num1){
+        resultado.innerHTML=(`${num2} é maior que ${num1}`)
+    }
+    else{
+        resultado.innerHTML=(`${num2} é igual a ${num1}`)
+    }
+}
+imprime()
